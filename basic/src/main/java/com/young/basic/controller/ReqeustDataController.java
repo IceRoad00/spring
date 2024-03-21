e087976884a1d04e1cd917dac248b99b716e357c
IceyoungK <anxis001220@gmail.com>
2024-03-21 17:15:59 +0900

    2024-03-21

--- basic/src/main/java/com/young/basic/controller/ReqeustDataController.java
+++ basic/src/main/java/com/young/basic/controller/ReqeustDataController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.young.basic.dto.SampleDto;
 
+import jakarta.validation.Valid;
+
 @RestController
 //HTTP * localhost:4000/request-data/**
 @RequestMapping("/request-data")
@@ -80,7 +82,8 @@ public class ReqeustDataController {
     @PostMapping("/post")
     public String post(
         // @RequestBody String text
-        @RequestBody SampleDto dto
+        // @Valid : 해당 payload에 대해서 유효성 검사를 실시하도록 함
+        @RequestBody @Valid SampleDto dto
     ) {
         return "전송한 데이터 : " + dto.toString();
     }
---
+++ basic/src/main/java/com/young/basic/controller/ResponseDataController.java
@@ -0,0 +1,49 @@
+package com.young.basic.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import com.young.basic.dto.ResponseSampleDto;
+
+@RestController
+// HTTP * localhost:4000/response-data/**
+@RequestMapping("/response-data")
+public class ResponseDataController {
+
+/*
+    @ResponseEntity:
+    - Reponse의 header, status code, status message, data를 조작할 수 있도록 하는 클래스
+*/    
+    @GetMapping("/{number}")
+    public ResponseEntity<String> getNumber(
+    @PathVariable("number") Integer number
+    ) {
+    // String body = number > 0 ? "양수" : number < 0 ? "음수" : "영";
+    // return new ResponseEntity<String>(body, HttpStatusCode.valueOf(400));
+    // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+
+    ResponseEntity<String> response = null;
+        
+    if(number < 0) 
+        response = ResponseEntity.status(202).body("음수");
+    if(number == 0) 
+        response = ResponseEntity.status(201).body("영");
+    if(number > 0) 
+        response = ResponseEntity.status(200).body("양수");
+        
+    return response;
+    }
+
+    @GetMapping("/response/dto")
+    public ResponseEntity<ResponseSampleDto> getResponseDto() {
+        ResponseEntity<ResponseSampleDto> response = 
+        ResponseEntity.status(201).body(new ResponseSampleDto(("문자열"), 99));
+        return response;
+    }
+}
