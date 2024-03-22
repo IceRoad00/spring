40c40d9e38fb7558cf2227394937758e0aa6453a
IceyoungK <anxis001220@gmail.com>
2024-03-22 17:10:05 +0900

    2024-03-22

--- basic/src/main/java/com/young/basic/controller/ResponseDataController.java
+++ basic/src/main/java/com/young/basic/controller/ResponseDataController.java
@@ -1,13 +1,10 @@
 package com.young.basic.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import com.young.basic.dto.ResponseSampleDto;
 
